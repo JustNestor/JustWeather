909b7f857ff475a07790c2f96c9eb3c23e9a1199
JustNestor <nestorkomarnitskiy@gmail.com>
2024-05-15 14:39:51 +0300

    Frontend code refactored

--- src/App.vue
+++ src/App.vue
@@ -2,143 +2,175 @@
 import { ref } from "vue";
 import { invoke } from "@tauri-apps/api/tauri";
 
-const city_search = ref("Lviv");
-const city_name = ref("");
-const city_country = ref("");
 
-const now = ref({"date": ["", "", "", "", ""], "temp": 0.0, "feelslike": 0.0, "pressure": 0, "humidity": 0, "visibility": 0, "sunrise": "HH:MM", "sunset": "HH:MM"});
-const hourlyforecast = ref({"forecast": [{"time": "HH:MM", "temp": 0.0}]});
-const fivedayforecast = ref({"forecast": [{"date": ["", "", "", "", ""], "temp": 0.0}]});
+const searchQuery = ref("Lviv");
+const cityName = ref("");
+const countryName = ref("");
+const currentWeather = ref({
+  date: ["", "", "", "", ""],
+  temp: 0.0,
+  feelslike: 0.0,
+  pressure: 0,
+  humidity: 0,
+  visibility: 0,
+  sunrise: "HH:MM",
+  sunset: "HH:MM"
+});
+
+const hourlyForecast = ref({ forecast: [{ time: "HH:MM", icon: "", temp: 0.0 }] });
+const fiveDayForecast = ref({ forecast: [{ date: ["", "", "", "", ""], icon:"", temp: 0.0 }] });
 
 
 
-async function search_city() {
-	let city = city_search.value;
+async function searchCity() {
+  const city = searchQuery.value;
 
-  let json = await invoke('search_city', {name: city}).then((data) => {
-    return JSON.parse(data.replaceAll("'", '"')).result;
-  });
+  const json = await invoke('search_city', { name: city })
+    .then((data) => JSON.parse(data.replaceAll("'", '"')).result);
 
-  city_country.value = json[0].country;
-  city_name.value = json[0].name;
+  countryName.value = json[0].country;
+  cityName.value = json[0].name;
 
-  parse_weather(json[0].lat, json[0].lon);
-  get_airindex(json[0].lat, json[0].lon);
+  parseWeather(json[0].lat, json[0].lon);
+  getAirIndex(json[0].lat, json[0].lon);
 }
 
-async function parse_weather(lat, lon) {
-  let json = await invoke('get_weather', {lat: lat, lon: lon}).then((data) => {
-    return JSON.parse(data.replaceAll("'", '"'));
-  });
 
-  now.value = json.now;
-  hourlyforecast.value = json.hourlyforecast;
-  fivedayforecast.value = json.fivedayforecast;
-  console.log(now.value, hourlyforecast.value, fivedayforecast.value);
+async function parseWeather(lat, lon) {
+  const json = await invoke('get_weather', { lat, lon })
+    .then((data) => JSON.parse(data.replaceAll("'", '"')));
+
+  currentWeather.value = json.now;
+  hourlyForecast.value = json.hourlyforecast;
+  fiveDayForecast.value = json.fivedayforecast;
 }
 
-async function get_airindex(lat, lon) {
-  let json = await invoke('get_airindex', {lat: lat, lon: lon}).then((data) => {
-    return JSON.parse(data.replaceAll("'", '"'));
-  });
+
+async function getAirIndex(lat, lon) {
+  const json = await invoke('get_airindex', { lat, lon })
+    .then((data) => JSON.parse(data.replaceAll("'", '"')));
 
   console.log(json);
-
 }
-
 
 </script>
 
+
 <template>
   <div class="container">
-
     <div class="topbar">
-      <span class="appname">JustWeather</span>
-      <div class="searchbar">
-        <input class="search" v-model="city_search" v-on:keyup.enter="search_city()" placeholder="Шукати місто.." type="text">
+      <span class="app-name">JustWeather</span>
+      <div class="search-bar">
+        <input
+          class="search-input"
+          v-model="searchQuery"
+          v-on:keyup.enter="searchCity()"
+          placeholder="Шукати місто..."
+          type="text"
+        />
+        <img src="./assets/icons/search.svg" class="search-icon" />
       </div>
     </div>
 
-    <div class="now">
-      <span class="nowtxt">Зараз</span>
-      <hr class="hr">
-      <div class="weathernow">
-        <div class="tempnow">{{parseInt(now.temp)}}°C</div>
-        <img class="iconnow" :src="'src/assets/weather_icons/' + now.icon + '.png'">
-        <div class="weathername">{{now.weather_name}}</div>
+    <div class="current-weather">
+      <span class="current-weather-text">Зараз</span>
+      <hr class="separator" />
+      <div class="weather-details">
+        <div class="current-temp">{{ parseInt(currentWeather.temp) }}°C</div>
+        <img
+          class="current-weather-icon"
+          :src="'src/assets/weather_icons/' + currentWeather.icon + '.png'"
+        />
+        <div class="weather-description">{{ currentWeather.weatherName }}</div>
       </div>
-      <div class="destination">
-        <div class="datenow">{{now.date[2]}} {{now.date[0]}}, {{now.date[3]}}</div>
-        <div class="place">{{city_name}}, {{city_country}}</div>
-        <img src="" class="calendar">
-        <img src="" class="">
-      </div>
-    </div>
-
-    <div class="nextforecast">
-      <div v-for="nf in fivedayforecast.forecast" class="day">
-        <div class="tempnext">{{parseInt(nf.temp)}}°</div>
-        <div class="datenext">{{nf.date[0]}} {{nf.date[3]}}</div>
-        <div class="daynext">{{nf.date[1]}}</div>
-        <img class="iconfloat" :src="'src/assets/weather_icons/' + nf.icon + '.png'">
-      </div>
-    </div>
-
-    <span class="forecasttxt">Прогноз на 5 днів</span>
-    <span class="todayattxt">Сьогодні в</span>
-
-    <div class="weathersurface" >
-      <span class="weathercondtxt">Погодні умови</span>
-
-      <div class="surf surf1">
-        <span class="airindex">Індекс якості повітря</span>
-        <div class="index">
-			
+      <div class="location-details">
+        <div class="current-date">
+          {{ currentWeather.date[2] }} {{ currentWeather.date[0] }},
+          {{ currentWeather.date[3] }}
         </div>
+        <div class="location">{{ cityName }}, {{ countryName }}</div>
+        <img src="./assets/icons/calendar.svg" class="calendar-icon" />
+        <img src="./assets/icons/my_location.svg" class="location-icon" />
+      </div>
+    </div>
+
+    <div class="five-day-forecast">
+      <div v-for="forecast in fiveDayForecast.forecast" class="day">
+        <div class="forecast-temp">{{ parseInt(forecast.temp) }}°</div>
+        <div class="forecast-date">
+          {{ forecast.date[0] }} {{ forecast.date[3] }}
+        </div>
+        <div class="forecast-day">{{ forecast.date[1] }}</div>
+        <img
+          class="forecast-icon"
+          :src="'src/assets/weather_icons/' + forecast.icon + '.png'"
+        />
+      </div>
+    </div>
+
+    <span class="forecast-text">Прогноз на 5 днів</span>
+    <span class="today-at-text">Сьогодні в</span>
+
+    <div class="weather-surface">
+      <span class="weather-conditions-text">Погодны умови</span>
+      <div class="weather-detail surf1">
+        <span class="air-quality-index">Індекс якості повітря</span>
+        <div class="index">Добре</div>
+        <img src="./assets/icons/wind.svg" class="air-index-icon" />
         <span class="pm">PM25</span>
         <span class="so">SO2</span>
         <span class="no">NO2</span>
-        <span  class="o3">O3</span>
+        <span class="o3">O3</span>
+        <span class="pm_ value">100</span>
+        <span class="so_ value">100</span>
+        <span class="no_ value">100</span>
+        <span class="o3_ value">100</span>
       </div>
 
-      <div class="surf surf2">
-        <span class="sunrisesunset">Схід та Захід</span>
-        <span class="sunrisetxt">Схід</span>
-        <span class="sunsettxt">Захід</span>
-        <span class="risetime">{{now.sunrise}}</span>
-        <span class="settime">{{now.sunset}}</span>
+      <div class="weather-detail surf2">
+        <span class="sunrise-sunset">Схід та Захід</span>
+        <span class="sunrise-text">Схід</span>
+        <span class="sunset-text">Захід</span>
+        <span class="rise-time">{{ currentWeather.sunrise }}</span>
+        <span class="set-time">{{ currentWeather.sunset }}</span>
+        <img src="./assets/icons/sun.svg" class="rise-time-icon" />
+        <img src="./assets/icons/moon.svg" class="set-time-icon" />
       </div>
 
-      <div class="surf surf3">
-        <span class="pressuretxt">Тиск</span>
-        <span class="pressure">{{now.pressure}}hPa</span> 
+      <div class="weather-detail surf3">
+        <span class="pressure-text">Тиск</span>
+        <span class="pressure-value">{{ currentWeather.pressure }}hPa</span>
+        <img src="./assets/icons/waves.svg" class="pressure-icon" />
       </div>
 
-      <div class="surf surf4">
-        <span class="humiditytxt">Вологість</span>
-        <span class="humidity">{{now.humidity}}%</span> 
+      <div class="weather-detail surf4">
+        <span class="humidity-text">Вологість</span>
+        <span class="humidity-value">{{ currentWeather.humidity }}%</span>
+        <img src="./assets/icons/humidity.svg" class="humidity-icon" />
       </div>
 
-      <div class="surf surf5">
-        <span class="visibilitytxt">Видимість</span>
-        <span class="visibility">{{now.visibility}}км</span> 
+      <div class="weather-detail surf5">
+        <span class="visibility-text">Видимість</span>
+        <span class="visibility-value">{{ currentWeather.visibility }}km</span>
+        <img src="./assets/icons/eye.svg" class="visibility-icon" />
       </div>
-      
-      <div class="surf surf6">
-        <span class="feelsliketxt">Відчувається як</span>
-        <span class="feelslike">{{parseInt(now.feelslike)}}°</span> 
+
+      <div class="weather-detail surf6">
+        <span class="feels-like-text">Відчувається як</span>
+        <span class="feels-like-value">{{ parseInt(currentWeather.feelslike) }}°</span>
+        <img src="./assets/icons/temp.svg" class="temp-icon" />
       </div>
     </div>
+  </div>
 
-    <div class="hourly">
-      <div v-for="h in hourlyforecast.forecast" class="hourlyweather">
-        <div class="temphourly">{{parseInt(h.temp)}}°</div>
-        <img class="hourlyicon" :src="'src/assets/weather_icons/' + h.icon + '.png'">
-        <div class="timehourly">{{h.time}}</div>
-      </div>
-    </div> 
-
+  <div class="hourly-forecast">
+    <div v-for="hourlyForecast in hourlyForecast.forecast" class="hourly-weather">
+      <div class="hourly-temp">{{ parseInt(hourlyForecast.temp) }}°</div>
+      <img
+        class="hourly-icon"
+        :src="'src/assets/weather_icons/' + hourlyForecast.icon + '.png'"
+      />
+      <div class="hourly-time">{{ hourlyForecast.time }}</div>
+    </div>
   </div>
 </template>
-
-
